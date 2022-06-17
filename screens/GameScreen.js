b92ff830d2785d3e757846dafe456809b92fddbb
Heshan <heshan.imedha@gmail.com>
2022-06-17 21:50:00 +0530

    Creating, Using & Displaying Random Numbers

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,14 +1,28 @@
+import { useState } from "react";
 import { Text, View, StyleSheet } from "react-native";
-import Title from "../components/Title";
+import NumberContainer from "../components/game/NumberContainer";
+import Title from "../components/ui/Title";
 
-function GameScreen() {
+function generateRandomBetween(min, max, exclude) {
+  const rndNum = Math.floor(Math.random() * (max - min)) + min;
+
+  if (rndNum === exclude) {
+    return generateRandomBetween(min, max, exclude);
+  } else {
+    return rndNum;
+  }
+}
+
+function GameScreen({ useNumber }) {
+  const initialGuess = generateRandomBetween(1, 100, useNumber);
+  const [currentGuess, setCurrentGuess] = useState(initialGuess);
   return (
     <View style={styles.screen}>
-    <Title>Opponent's Guess</Title>
+      <Title>Opponent's Guess</Title>
+      <NumberContainer>{currentGuess}</NumberContainer>
       <View>
         <Text>Higher or Lower?</Text>
       </View>
-      {/* <View>LOG ROUNDS</View> */}
     </View>
   );
 }
@@ -20,5 +34,4 @@ const styles = StyleSheet.create({
     flex: 1,
     padding: 24,
   },
- 
 });
